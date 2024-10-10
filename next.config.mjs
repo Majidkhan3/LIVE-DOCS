4c5191592a86960b3001f4c154fff16e9145b084
Majidkhan3 <majidabid017@gmail.com>
2024-10-10 19:49:44 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,9 @@ const nextConfig = {
       },
     ],
   },
-  // These settings are necessary for GitHub Pages deployment
   basePath: isProd ? "/LIVE-DOCS" : "",
   assetPrefix: isProd ? "/LIVE-DOCS/" : "",
-  trailingSlash: true, // Ensures each page ends with a trailing slash, needed for static export
+  trailingSlash: true, // Ensures compatibility with GitHub Pages
 };
 
 export default nextConfig;
