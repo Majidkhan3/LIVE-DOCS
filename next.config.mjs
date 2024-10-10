4bbe3d6e8405990d7e4ca8f0532ee83649ac8e5c
Majidkhan3 <majidabid017@gmail.com>
2024-10-10 19:47:17 +0500

    changes of worlflow in Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "img.clerk.com" }],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
   },
+  // These settings are necessary for GitHub Pages deployment
+  basePath: isProd ? "/LIVE-DOCS" : "",
+  assetPrefix: isProd ? "/LIVE-DOCS/" : "",
+  trailingSlash: true, // Ensures each page ends with a trailing slash, needed for static export
 };
 
 export default nextConfig;
